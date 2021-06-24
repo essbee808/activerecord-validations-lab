37aa3c9948527edb8f00736a1891d46948fbfcce
essbee808 <sheena.myb@gmail.com>
2021-06-23 21:59:05 -0700

    create custom validation to post

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,4 @@
 class Author < ActiveRecord::Base
-    validates :name, presence: true
-    validates :name, uniqueness: true
+    validates :name, presence: true, uniqueness: true
     validates :phone_number, length: { is: 10 }
-    
-    include ActiveModel::Validations
-    validates_with PostValidator
 end
